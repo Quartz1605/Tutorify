8e0695ccf96a3afe7be8d3ee8b5c0794c5ef4c47
Quartz1605 <prathambarekal05@gmail.com>
2025-06-21 20:38:22 +0530

    Made database models for Tutors

--- backend/Tutorify/tutors_auth/admin.py
+++ backend/Tutorify/tutors_auth/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import TutorDetails,TutorIntroduction,TutorPreferences,TutorProfile
 
 # Register your models here.
+
+admin.site.register(TutorDetails)
+admin.site.register(TutorIntroduction)
+admin.site.register(TutorPreferences)
+admin.site.register(TutorProfile)
+
+
+
--- backend/Tutorify/tutors_auth/models.py
+++ backend/Tutorify/tutors_auth/models.py
@@ -12,7 +12,32 @@ class TutorProfile(models.Model):
   date_of_birth = models.CharField(max_length=20)
   gender = models.CharField(max_length=10)
   address = models.TextField()
+  profile_picture = models.ImageField(upload_to='tutor_images/')
+
+class TutorDetails(models.Model):
+
+  tutor = models.ForeignKey(TutorProfile,on_delete=models.CASCADE)
+  highest_qualification = models.CharField(max_length=100)
+  subjects_taught = models.CharField(max_length=100)
+  teaching_experience = models.CharField(max_length=50)
+  language_spoken = models.CharField(max_length=50)
   
+
+class TutorPreferences(models.Model):
+
+  tutor = models.ForeignKey(TutorProfile,on_delete=models.CASCADE)
+  teaching_mode = models.CharField(max_length=20)
+  age_group = models.CharField(max_length=30)
+  travel_preference = models.CharField(max_length=10)
+  fee_range = models.CharField(max_length=10)
+
+class TutorIntroduction(models.Model):
+
+  tutor = models.ForeignKey(TutorProfile,on_delete=models.CASCADE)
+  bio = models.TextField()
+  tagline = models.CharField(max_length=100)
+  video = models.FileField(upload_to='tutor-intro/')
+  resume = models.FileField(upload_to='tutors-resume/')
 
 
 
